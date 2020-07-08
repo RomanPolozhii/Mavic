f4ba2695c83b787c96eb78954c846a8d8be659f2
RomanPolozhii <roman.polozhii@gmail.com>
2020-07-08 20:36:11 +0300

    added product section

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,12 @@ $(function () {
   }
 
   ibg();
+
+  $(".product__slider").slick({
+    infinite: false,
+    nextArrow:
+      '<button type="button" class="product__slider-arrows product__slider-right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
+    prevArrow:
+      '<button type="button" class="product__slider-arrows product__slider-left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
+  });
 });
